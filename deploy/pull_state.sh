46e5de7b12222c010cf58e4bd3dc1e0a62ede190
Andrii Khovaiev <5860596+jigsawyer@users.noreply.github.com>
2026-07-13 20:54:52 +0300

    pull state

--- deploy/pull_state.sh
+++ deploy/pull_state.sh
@@ -7,7 +7,7 @@ HA_URL="http://192.168.1.181:8123"
 HA_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxOTgxMWU4MmM1YWY0NzQ4ODkyNjIyNDM0M2NiOGE3MiIsImlhdCI6MTc4Mzk2NDE2OSwiZXhwIjoyMDk5MzI0MTY5fQ.ksCyuH_KmPWQA6OOzlrLiqgRSRIlV-byNrBtIIw_f8Q"
 
 cd $REPO_DIR
-git fetch origin main
+git fetch origin master
 
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse @{u})
@@ -16,7 +16,7 @@ if [ $LOCAL = $REMOTE ]; then
     exit 0
 fi
 
-git reset --hard origin/main
+git reset --hard origin/master
 git clean -fd
 
 # Trigger API to apply State
