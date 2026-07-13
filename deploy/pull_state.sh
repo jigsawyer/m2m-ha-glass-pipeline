d3044af3de1b4183cb5ef00f2bfc90f2e4657be3
Andrii Khovaiev <5860596+jigsawyer@users.noreply.github.com>
2026-07-13 20:53:26 +0300

    pull state

--- deploy/pull_state.sh
+++ deploy/pull_state.sh
@@ -2,7 +2,7 @@
 # Path: /config/m2m-pipeline/deploy/pull_state.sh
 set -e
 
-REPO_DIR="/config/m2m-pipeline"
+REPO_DIR="/config/m2m-ha-glass-pipeline"
 HA_URL="http://192.168.1.181:8123"
 HA_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxOTgxMWU4MmM1YWY0NzQ4ODkyNjIyNDM0M2NiOGE3MiIsImlhdCI6MTc4Mzk2NDE2OSwiZXhwIjoyMDk5MzI0MTY5fQ.ksCyuH_KmPWQA6OOzlrLiqgRSRIlV-byNrBtIIw_f8Q"
 
